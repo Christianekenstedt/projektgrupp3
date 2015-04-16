d8a8e32c01bf01b0f3690d1a344dea4475a9416a
Christianekenstedt <Christian@n144-p41.kthopen.kth.se>
2015-04-16 14:46:44 +0200

    Nytt projekt

---
+++ prototyp1/prototyp1/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  prototyp1
+//
+//  Created by Christian Ekenstedt on 2015-04-16.
+//  Copyright (c) 2015 Grupp 3. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
