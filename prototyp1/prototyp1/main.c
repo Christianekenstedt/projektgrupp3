7c9cdcad7dbc9fa2e648450702f07e7e033d7a2c
Christianekenstedt <Christian@n144-p41.kthopen.kth.se>
2015-04-16 15:50:11 +0200

    Massa nytt

--- prototyp1/prototyp1/main.c
+++ prototyp1/prototyp1/main.c
@@ -11,48 +11,136 @@
 #include <SDL2_net/SDL_net.h>
 #include <SDL2_image/SDL_image.h>
 #include <stdio.h>
+#include <stdbool.h>
 
 //Screen dimension constants
 const int SCREEN_WIDTH = 640;
 const int SCREEN_HEIGHT = 480;
 
+//Starts up SDL and creates window
+bool init();
+
+//Loads media
+bool loadMedia();
+
+//Frees media and shuts down SDL
+void closeW();
+
+//The window we'll be rendering to
+SDL_Window* gWindow = NULL;
+
+//The surface contained by the window
+SDL_Surface* gScreenSurface = NULL;
+
+//The image we will load and show on the screen
+SDL_Surface* gXOut = NULL;
+
 int main( int argc, char* args[] )
 {
-    //The window we'll be rendering to
-    SDL_Window* window = NULL;
+    //Start up SDL and create window
+    if( !init() )
+    {
+        printf( "Failed to initialize!\n" );
+    }
+    else
+    {
+        //Load media
+        if( !loadMedia() )
+        {
+            printf( "Failed to load media!\n" );
+        }
+        else
+        {
+            //Main loop flag
+            bool quit = false;
+            
+            //Event handler
+            SDL_Event e;
+            
+            //While application is running
+            while( !quit )
+            {
+                //Handle events on queue
+                while( SDL_PollEvent( &e ) != 0 )
+                {
+                    //User requests quit
+                    if( e.type == SDL_QUIT )
+                    {
+                        quit = true;
+                    }
+                }
+            
+                //Apply the image
+                SDL_BlitSurface( gXOut, NULL, gScreenSurface, NULL );
+                //Update the surface
+                SDL_UpdateWindowSurface( gWindow );
+            
+            }
+        }
+    }
     
-    //The surface contained by the window
-    SDL_Surface* screenSurface = NULL;
+    //Free resources and close SDL
+    closeW();
+    
+    return 0;
+}
+
+bool init()
+{
+    //Initialization flag
+    bool success = true;
     
     //Initialize SDL
     if( SDL_Init( SDL_INIT_VIDEO ) < 0 )
     {
         printf( "SDL could not initialize! SDL_Error: %s\n", SDL_GetError() );
-    }else{
+        success = false;
+    }
+    else
+    {
         //Create window
-        window = SDL_CreateWindow( "SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN );
-        if( window == NULL )
+        gWindow = SDL_CreateWindow( "Hello, World!", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN );
+        if( gWindow == NULL )
         {
             printf( "Window could not be created! SDL_Error: %s\n", SDL_GetError() );
-        }else{
+            success = false;
+        }
+        else
+        {
             //Get window surface
-            screenSurface = SDL_GetWindowSurface( window );
-            
-            //Fill the surface white
-            SDL_FillRect( screenSurface, NULL, SDL_MapRGB( screenSurface->format, 0xFF, 0xFF, 0xFF ) );
-            
-            //Update the surface
-            SDL_UpdateWindowSurface( window );
-            
-            //Wait two seconds
-            SDL_Delay( 2000 );
+            gScreenSurface = SDL_GetWindowSurface( gWindow );
         }
     }
+    
+    return success;
+}
+
+bool loadMedia()
+{
+    //Loading success flag
+    bool success = true;
+    
+    //Load splash image
+    gXOut = IMG_Load( "bilder/bild.png" );
+    if( gXOut == NULL )
+    {
+        printf( "Unable to load image %s! SDL Error: %s\n", "bilder/bild.png", SDL_GetError() );
+        success = false;
+    }
+    
+    return success;
+}
+
+void closeW()
+{
+    //Deallocate surface
+    SDL_FreeSurface( gXOut );
+    gXOut = NULL;
+    
     //Destroy window
-    SDL_DestroyWindow( window );
+    SDL_DestroyWindow( gWindow );
+    gWindow = NULL;
     
     //Quit SDL subsystems
     SDL_Quit();
-    
-    return 0;
 }
---
+++ prototyp1/prototyp1/server.c
@@ -0,0 +1,17 @@
+//
+//  server.c
+//  prototyp1
+//
+//  Created by Christian Ekenstedt on 2015-04-16.
+//  Copyright (c) 2015 Grupp 3. All rights reserved.
+//
+
+#include <SDL2/SDL.h>
+#include <SDL2_net/SDL_net.h>
+#include <SDL2_image/SDL_image.h>
+#include <stdio.h>
+#include <stdbool.h>
+
+int main( int argc, char* args[] ){
+    
+}
