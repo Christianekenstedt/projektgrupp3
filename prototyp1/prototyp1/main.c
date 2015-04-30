e906ac1523236b311d2f2dd4b1d1aeeba28959c7
Christianekenstedt <Christian@Christians-MacBook-Pro.local>
2015-04-30 10:46:12 +0200

    nytt

--- prototyp1/prototyp1/main.c
+++ prototyp1/prototyp1/main.c
@@ -9,16 +9,7 @@
 #include "multiOS.h"
 #include <stdio.h>
 #include <stdbool.h>
-#include <string.h><svbn
-
-
-#include "SDL.h"
-#include "SDL_net.h"
-#include "SDL_image.h"
-#include "SDL_mixer.h"
-#include "SDL_ttf.h"
-#include "SDL_timer.h"
-#include "SDL_thread.h"
+#include <string.h>
 
 //Screen dimension constants
 const int SCREEN_WIDTH = 1280;
