906974ce4ae8bfd809b9ef26c2ffe0366ea4aaad
swedden <swedden94@gmail.com>
2015-05-06 12:50:50 +0200

    oskadad server, igen

--- prototyp1/prototyp1/server.c
+++ prototyp1/prototyp1/server.c
@@ -12,56 +12,12 @@ typedef struct stringinfo{
     //Kort kortlek;
 }sinfo;
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-static int threadFunction(void *ptr){
-
-    struct thread *p = (struct thread *) ptr;
-    //TCPsocket csd = p->csd;
-
-    int i=0, quit2=0;
-    char buffer[512];
-    for(i=0; i<512; i++){
-        buffer[i]='\0';
-    }
-    printf("INNE I THREADFUNC\n");
-    while(!quit2){
-        if (SDLNet_TCP_Recv(csd, buffer, 512) > 0)
-        {
-            printf("Client say: %s\n", buffer);
-
-            if(strcmp(buffer, "exit") == 0)	/* Terminate this connection */
-            {
-                quit2 = 1;
-                printf("Terminate connection\n");
-            }
-
-            if(strcmp(buffer, "quit") == 0)	/* Quit the program */
-            {
-                quit2 = 1;
-                //quit = 1;
-                printf("Quit program\n");
-            }
-            if (SDLNet_TCP_Send(csd, buffer, 512) < 0){
-                fprintf(stderr, "SDLNet_TCP_Send: %s\n", SDLNet_GetError());
-            }
-        }else if (SDLNet_TCP_Recv(csd, buffer, 512) < 0){
-            fprintf(stderr, "SDLNet_TCP_Recv: %s\n", SDLNet_GetError());
-            quit2=1;
-        }
-    }//wile
-    return quit2;
-}//funktion
-=======
-=======
 //void fyllTradar(int value, sinfo clientvalue[]);
 
 Kort kortlek[ANTALKORT];
 int playerturn = -1; // FÖrlåt jonas <3 (-1 = ledig)
 
->>>>>>> 60c027bdf1f8e643c65a1b382da5b9d5035bd394
 SDL_ThreadFunction* function(void* incsocket);
->>>>>>> 7ce2e83f1d585956ce3bec9a01b25172ed5f26ee
 
 void gameInit(Kort kortlek[]){
     initiera_kortleken(kortlek);
@@ -72,25 +28,7 @@ void gameInit(Kort kortlek[]){
 
 int main (int argc, char *argv[])
 {
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-    TCPsocket sd; //csd; /* Socket descriptor, Client socket descriptor */
-    IPaddress ip, *remoteIP;
-    SDL_Thread *thread;
-    Thread parametrar;
-    int quit, quit2, threadReturnValue;
-
-    if (SDLNet_Init() < 0)
-=======
-    
-    TCPsocket Listensock, Clientsock[10];
-=======
-
-=======
->>>>>>> 60c027bdf1f8e643c65a1b382da5b9d5035bd394
     TCPsocket Listensock, Clientsock[MAXCLIENTS];
->>>>>>> 3fda9495a90b586452bf3b8d54a44b86ef229d42
     IPaddress* ip;
     sinfo clientvalue[MAXCLIENTS];
     int quit = 0, ClientNumber=0;
@@ -104,100 +42,22 @@ int main (int argc, char *argv[])
 
 /* ########################## NÄTVERKS INIT, INKL ÖPPNA SOCKET ######################################## */
     if(SDLNet_Init() < 0)
->>>>>>> 7ce2e83f1d585956ce3bec9a01b25172ed5f26ee
     {
         fprintf(stderr, "SDLNet_Init: %s\n", SDLNet_GetError());
         exit(EXIT_FAILURE);
     }
-<<<<<<< HEAD
-<<<<<<< HEAD
 
-    /* Resolving the host using NULL make network interface to listen */
-    if (SDLNet_ResolveHost(&ip, NULL, 2000) < 0)
-=======
-    
-=======
-
->>>>>>> 3fda9495a90b586452bf3b8d54a44b86ef229d42
     if(SDLNet_ResolveHost(&ip, NULL, 2000) < 0)
->>>>>>> 7ce2e83f1d585956ce3bec9a01b25172ed5f26ee
     {
         fprintf(stderr, "SDLNet_ResolveHost: %s\n", SDLNet_GetError());
         exit(EXIT_FAILURE);
     }
-<<<<<<< HEAD
-<<<<<<< HEAD
 
-    /* Open a connection with the IP provided (listen on the host's port) */
-    if (!(sd = SDLNet_TCP_Open(&ip)))
-=======
-    
-=======
-
->>>>>>> 3fda9495a90b586452bf3b8d54a44b86ef229d42
     if(((Listensock = SDLNet_TCP_Open(&ip)) == NULL))
->>>>>>> 7ce2e83f1d585956ce3bec9a01b25172ed5f26ee
     {
         fprintf(stderr, "SDLNet_TCP_Open: %s\n", SDLNet_GetError());
         exit(EXIT_FAILURE);
     }
-<<<<<<< HEAD
-<<<<<<< HEAD
-
-    /* Wait for a connection, send data and term */
-    quit = 0;
-    while (!quit)
-=======
-    
-    ClientNumber = 0;
-    
-    while(ClientNumber < 10)
->>>>>>> 7ce2e83f1d585956ce3bec9a01b25172ed5f26ee
-    {
-        if((Clientsock[ClientNumber] = SDLNet_TCP_Accept(Listensock)))
-        {
-<<<<<<< HEAD
-
-            /* Now we can communicate with the client using csd socket
-             * sd will remain opened waiting other connections */
-
-            /* Get the remote address */
-            if ((remoteIP = SDLNet_TCP_GetPeerAddress(csd)))
-            /* Print the address, converting in the host format */
-                printf("Host connected: %x %d\n", SDLNet_Read32(&remoteIP->host), SDLNet_Read16(&remoteIP->port));
-            else
-                fprintf(stderr, "SDLNet_TCP_GetPeerAddress: %s\n", SDLNet_GetError());
-
-            quit2 = 0;
-            while (!quit2)
-            {
-                /* TRÅDFUNKTION BEHÖVS HÄR*/
-                printf("INNE I WHILE :)\n");
-                thread = SDL_CreateThread(threadFunction, "TestThread", &parametrar);
-                if (NULL == thread) {
-                    printf("\nSDL_CreateThread failed: %s\n", SDL_GetError());
-                } else {
-                    SDL_WaitThread(thread, &threadReturnValue);
-                    quit2= threadReturnValue;
-                    quit = quit2;
-                }
-            }
-
-            /* Close the client socket */
-            SDLNet_TCP_Close(csd);
-        }
-    }
-
-    SDLNet_TCP_Close(sd);
-    SDLNet_Quit();
-
-    return EXIT_SUCCESS;
-=======
-            motherfucker[ClientNumber].quit = &quit;
-            motherfucker[ClientNumber].clientnumber = ClientNumber;
-            motherfucker[ClientNumber].socket = &Clientsock[ClientNumber];
-            SDL_DetachThread(SDL_CreateThread(function, "TheThreadOfDoom", (void*)&motherfucker[ClientNumber]));
-=======
 
     for(i=0;i<MAXCLIENTS;i++){
         clientvalue[i].clientsocket=0;
@@ -258,14 +118,6 @@ int main (int argc, char *argv[])
         }
         else if(clientvalue[ClientNumber].clientsocket == 1)
         {
-<<<<<<< HEAD
-            clientvalue[ClientNumber].quit = &quit;
-            clientvalue[ClientNumber].clientnumber = ClientNumber;
-            clientvalue[ClientNumber].socket = &Clientsock[ClientNumber];
-            SDL_DetachThread(SDL_CreateThread(function, "Client", (void*)&clientvalue[ClientNumber]));
->>>>>>> 3fda9495a90b586452bf3b8d54a44b86ef229d42
-=======
->>>>>>> 60c027bdf1f8e643c65a1b382da5b9d5035bd394
             ClientNumber++;
             printf("ClienNumber increased to: %d from %d due to full\n", ClientNumber,ClientNumber-1);
         }
@@ -432,7 +284,6 @@ SDL_ThreadFunction* function(void* incsocket)
         }
     }
     return 0;
->>>>>>> 7ce2e83f1d585956ce3bec9a01b25172ed5f26ee
 }
 
 /*void fyllTradar(int value, sinfo clientvalue[]){
